dbfed3e5b0cf255cbf0d9a965b20391e573ea1a3
Edsko de Vries <edsko@well-typed.com>
2021-02-17 12:06:12 +0100

    Add test for nested pattern matches
    This now works, and in particular, this now works as well:
    
    ```haskell
    largeRecord defaultPureScript [d|
        data T a = MkT { x :: Int,  y :: [a] }
        data S a = MkS { x :: Char, y :: T a }
      |]
    ```
    
    That wasn't working before, I think due to the less careful treatment of names.

--- test/Test/Record/Sanity/PatternMatch.hs
+++ test/Test/Record/Sanity/PatternMatch.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE QuasiQuotes     #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
 
 module Test.Record.Sanity.PatternMatch (tests) where
 
@@ -9,7 +10,8 @@ import Test.Tasty.HUnit
 import Data.Record.TH
 
 largeRecord defaultPureScript [d|
-    data T a = MkT { x :: Int, y :: [a] }
+    data T a = MkT { x :: Int,  y :: [a] }
+    data S a = MkS { x :: Char, y :: T a }
   |]
 
 endOfBindingGroup
@@ -20,13 +22,20 @@ projectOne [lr| MkT { x = a } |] = a
 projectTwo :: T a -> (Int, [a])
 projectTwo [lr| MkT { x = a, y = b } |] = (a, b)
 
+projectNested :: S a -> (Char, Int, [a])
+projectNested [lr| MkS { x = a, y = MkT { x = b, y = c } } |] = (a, b, c)
+
 testProjections :: Assertion
 testProjections = do
-    assertEqual "projectOne" (projectOne t) 5
+    assertEqual "projectOne" (projectOne t)  5
     assertEqual "projectTwo" (projectTwo t) (5, [True])
+    assertEqual "projectNested" (projectNested s) ('a', 2, [True, False])
   where
     t :: T Bool
     t = [lr| MkT { x = 5, y = [True] } |]
+
+    s :: S Bool
+    s = [lr| MkS { x = 'a', y = MkT { x = 2, y = [True, False] } } |]
 
 tests :: TestTree
 tests = testGroup "Test.Record.Sanity.Projection" [

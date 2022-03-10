dc9ff3615104de058f2c7ad1a937c8f2fd42dd06
Edsko de Vries <edsko@well-typed.com>
2022-03-10 13:03:58 +0100

    Add comment about MetadataOf
    This used to live in the codegen module, but there nobody will see it.

--- large-generics/src/Data/Record/Generic.hs
+++ large-generics/src/Data/Record/Generic.hs
@@ -46,6 +46,19 @@ class Generic a where
   type Constraints a :: (Type -> Constraint) -> Constraint
 
   -- | Type-level metadata
+  --
+  -- NOTE: using type-level lists without resulting in quadratic core code is
+  -- extremely difficult. Any use of this type-level metadata therefore needs
+  -- delibrate consideration. Some examples:
+  --
+  -- o Within the @large-generics@ library, 'MetadataOf' is used in the
+  --   definition of 'HasNormalForm'. This constraint is carefully defined to
+  --   avoid quadratic code, as described in the presentation
+  --   "Avoiding Quadratic Blow-up During Compilation"
+  --   <https://skillsmatter.com/skillscasts/17262-avoiding-quadratic-blow-up-during-compilation>
+  -- o The @large-records@ library uses it to provide a compatibility layer
+  --   between it and @sop-core@; this is however only for testing purposes, and
+  --   the quadratic code here is simply accepted.
   type MetadataOf a :: [(Symbol, Type)]
 
   -- | Translate to generic representation

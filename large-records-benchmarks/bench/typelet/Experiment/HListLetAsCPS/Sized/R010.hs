2233928cd1306ca848ee94cc4cf52e20084d23ba
Edsko de Vries <edsko@well-typed.com>
2022-03-24 18:12:22 +0100

    Simplify the HList-using-CPS benchmark
    There is no need to specify the full type up-front (ghc can infer it),
    and we were not using the type variables.

--- large-records-benchmarks/bench/typelet/Experiment/HListLetAsCPS/Sized/R010.hs
+++ large-records-benchmarks/bench/typelet/Experiment/HListLetAsCPS/Sized/R010.hs
@@ -16,16 +16,16 @@ import Bench.Types
 import Common.HListOfSize.HL010
 
 hlist :: HList Fields
-hlist = letT' (Proxy @Fields) $ \(_ :: Proxy r) -> castEqual $
+hlist = letT' Proxy $ \(_ :: Proxy r) -> castEqual $
     -- 09 .. 00
-    letAs' @(HList r) (MkT 09 :* Nil)  $ \(xs09 :: HList t09) ->
-    letAs' @(HList r) (MkT 08 :* xs09) $ \(xs08 :: HList t08) ->
-    letAs' @(HList r) (MkT 07 :* xs08) $ \(xs07 :: HList t07) ->
-    letAs' @(HList r) (MkT 06 :* xs07) $ \(xs06 :: HList t06) ->
-    letAs' @(HList r) (MkT 05 :* xs06) $ \(xs05 :: HList t05) ->
-    letAs' @(HList r) (MkT 04 :* xs05) $ \(xs04 :: HList t04) ->
-    letAs' @(HList r) (MkT 03 :* xs04) $ \(xs03 :: HList t03) ->
-    letAs' @(HList r) (MkT 02 :* xs03) $ \(xs02 :: HList t02) ->
-    letAs' @(HList r) (MkT 01 :* xs02) $ \(xs01 :: HList t01) ->
-    letAs' @(HList r) (MkT 00 :* xs01) $ \(xs00 :: HList t00) ->
+    letAs' @(HList r) (MkT 09 :* Nil)  $ \xs09 ->
+    letAs' @(HList r) (MkT 08 :* xs09) $ \xs08 ->
+    letAs' @(HList r) (MkT 07 :* xs08) $ \xs07 ->
+    letAs' @(HList r) (MkT 06 :* xs07) $ \xs06 ->
+    letAs' @(HList r) (MkT 05 :* xs06) $ \xs05 ->
+    letAs' @(HList r) (MkT 04 :* xs05) $ \xs04 ->
+    letAs' @(HList r) (MkT 03 :* xs04) $ \xs03 ->
+    letAs' @(HList r) (MkT 02 :* xs03) $ \xs02 ->
+    letAs' @(HList r) (MkT 01 :* xs02) $ \xs01 ->
+    letAs' @(HList r) (MkT 00 :* xs01) $ \xs00 ->
       castEqual xs00

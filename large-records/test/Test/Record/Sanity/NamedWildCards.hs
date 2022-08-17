84867a898751ba9b302e2e786640925a077eac86
kana <andrew4chrome@gmail.com>
2022-08-18 00:32:23 +0400

    Add test for NamedWildCards

---
+++ large-records/test/Test/Record/Sanity/NamedWildCards.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE KindSignatures        #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE UndecidableInstances  #-}
+{-# LANGUAGE NamedWildCards        #-}
+{-# LANGUAGE PolyKinds             #-}
+
+{-# OPTIONS_GHC -fplugin=Data.Record.Plugin #-}
+
+module Test.Record.Sanity.NamedWildCards () where
+
+{-# ANN type X largeRecord #-}
+data X = MkX {}
+
+{-# ANN type Y largeRecord #-}
+data Y a = MkY {}
+
+{-# ANN type Z largeRecord #-}
+data Z a (b :: a) = MkZ {}
